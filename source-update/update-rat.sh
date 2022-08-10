5acfca46d7f59e65ce993471d68a4b1b8a37de9d
Jonas.Stoehr <jonas.stoehr@nhochdrei.de>
2022-08-10 11:13:48 +0200

    Revert "Update update-rat.sh"
    - restore old format because the EU apparently cannot decide on how they
      output their test list
    
    This reverts commit 10fc9182d90bc674a4b4848ff323b97147ae7a6d.
    This reverts commit 6d1c70bcb1a12cd646e9941ac1d9e0975482fab8.

--- source-update/update-rat.sh
+++ source-update/update-rat.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-curl --silent --show-error 'https://covid-19-diagnostics.jrc.ec.europa.eu/devices/hsc-common-recognition-rat' | jq '{
+curl --silent --show-error https://covid-19-diagnostics.jrc.ec.europa.eu/devices/hsc-common-recognition-rat | jq \
+'{
   "valueSetId": "covid-19-lab-test-manufacturer-and-name",
+  "valueSetDate": .extracted_on[0:10],
   "valueSetValues":
-(reduce .[] as $i ({}; .[$i.id_device] =
+(reduce .deviceList[] as $i ({}; .[$i.id_device] =
 ($i.hsc_list_history | sort_by(.list_date)[-1]) as $latest |
 {
   "display": ($i.manufacturer.name + ", " + $i.commercial_name),
